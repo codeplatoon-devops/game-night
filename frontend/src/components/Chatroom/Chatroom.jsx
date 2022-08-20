d1ce9cfe3da8f57ed1a65ba451bc39acbc2cfc0f
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2022-08-20 16:59:33 -0400

    have how to send a message down at the bottom

--- frontend/src/components/Chatroom/Chatroom.jsx
+++ frontend/src/components/Chatroom/Chatroom.jsx
@@ -74,3 +74,12 @@ function Chatroom ({user}) {
 }
 
 export default Chatroom
+
+
+
+// How to send a message:
+// const message = await channel.sendMessage({
+//     text: "Did you already see the trailer? https://www.youtube.com/watch?v=wA38GCX4Tb0",
+//   });
+  
+//   return message;
