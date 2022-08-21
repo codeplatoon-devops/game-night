ef2e361eebf0cd5ab79ef0c6858e6898b61b348a
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-08-21 18:46:16 -0500

    RBT-13 added and modified models

--- backend/game_night_app/models.py
+++ backend/game_night_app/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-class User(AbstractUser):
+# requires 'pip install django-localflavor'
+# INSTALLED_APPS = (
+    # ...
+   # 'localflavor',
+# )
+# from localflavor.models import USStateField
+
+class AppUser(AbstractUser):
     email = models.EmailField(
         verbose_name='email address',
         max_length=64,
         unique=True,
     )
-    username=models.CharField(max_length=16)
+    username=models.CharField(max_length=16, verbose_name="Username")
     first_name=models.CharField(max_length=16, verbose_name="First name")
     last_name=models.CharField(max_length=16, verbose_name="Last name")
 
@@ -40,10 +47,18 @@ class Event(models.Model):
     def __str__(self):
         return f"ID: {self.id}, Category: {self.category}, Location: {self.location}, DateTime: {self.date_time}, Private: {self.private}"
 
-############################################################
-# Do we want a separate model for the location? Django doesn't have an 'address' field type
-#class Location(models.Model):
-############################################################
+# do we want to use from localflavor.models import USStateField or just a 2-character CharField?
+# do we also want to use localflavor.us.models.USZipCodeField?
+# allow any other fields to be null/blank?
+class Address(models.Model):
+    address_1 = models.CharField(max_length=128, verbose_name="Address")
+    address_2 = models.CharField( max_length=128, blank=True, null=True, default=None, verbose_name="Address cont'd")
+    city = models.CharField(max_length=64, verbose_name="City")
+    state = models.CharField(max_length=2, verbose_name="State")
+    zip_code = models.CharField(max_length=5, verbose_name="Zip code")
+
+    def __str__(self):
+        return f"ID: {self.id}, Address: {self.address_1}, {self.address_2}, City: {self.city}, State: {self.state}, Zip code: {self.zip_code}"
 
 class Game(models.Model):
     title=models.CharField(max_length=32, blank=False, unique=True, verbose_name="Title")
@@ -54,24 +69,35 @@ class Game(models.Model):
         return f"ID: {self.id}, Title: {self.title}"
 
 class EventGame(models.Model):
-    game=models.ForeignKey(Game, on_delete=models.CASCADE)
-    event=models.ForeignKey(Event, on_delete=models.CASCADE)
+    game=models.ForeignKey(Game, on_delete=models.CASCADE, related_name='games') # through='Game'?
+    event=models.ForeignKey(Event, on_delete=models.CASCADE, related_name='eventgames') # through='Event'?
 
     def __str__(self):
         return f"ID: {self.id}, Game: {self.game}, Event: {self.event}"
 
 ## Not sure I interpreted the requirements for this model correctly?
 class EventUser(models.Model):
-    owner=models.ForeignKey(User, on_delete=models.CASCADE)
-    attendee=models.ManyToManyField(User, related_name="eventusers")
-    event=models.ManyToManyField(Event, related_name="eventusers")
+    owner=models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    attendee=models.ManyToManyField(AppUser, related_name="attendees") # through='User'?
+    event=models.ManyToManyField(Event, related_name="events") # through='Event'?
 
     def __str__(self):
         return f"ID: {self.id}, Owner: {self.owner}, Event: {self.event}"
 
+class GroupList(models.Model):
+    user = models.OneToOneField(AppUser, on_delete = models.CASCADE, related_name='owner')
+    groups = models.ManyToManyField(AppUser, blank=True, related_name='groups')
+
+class GroupRequest(models.Model):
+    sender = models.ForeignKey(AppUser, on_delete = models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(AppUser, on_delete = models.CASCADE, related_name='receiver')
+    is_active = models.BooleanField(blank=True, default=True)
+    class Meta:
+        unique_together = (('sender', 'receiver'))
+
 # fields returned from https://api.boardgameatlas.com/api/
 class GameDetail(models.Model):
-    id=models.CharField(max_length = 10, unique=True, verbose_name="ID") # primary_key=True?
+    gameid=models.CharField(max_length = 10, unique=True, verbose_name="Game ID") # primary_key=True?
     handle=models.CharField(max_length = 32, blank=True, null=True, default=None, verbose_name="Handle") # required?
     url=models.URLField(max_length = 64, blank=True, null=True, default=None, verbose_name="URL") #CharField instead?
     edit_url=models.URLField(max_length = 64, blank=True, null=True, default=None, verbose_name="Edit URL") #CharField instead?
