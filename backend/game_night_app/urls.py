e0afa065b8d8cd18d79ce68ffd531f05b4052581
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2022-08-22 16:31:26 -0400

    still showed a diff in package.json

--- backend/game_night_app/urls.py
+++ backend/game_night_app/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.send_the_homepage),
     path('chattoken', views.create_chat_user_token),
-    path('/grouprequest/create', views.create_group_request),
-    path('/grouprequest/view', views.view_group_request),
+    path('grouprequest/create', views.create_group_request),
+    path('grouprequest/view', views.view_group_request),
     path('logout', views.log_out),
     path('games', views.bga_games),
 ]
