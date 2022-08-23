2553262aefa77394c9c234b47cb7f031172277b3
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-08-23 17:41:54 +0200

    login fixed, not refreshing

--- frontend/src/components/Forms/LogInForm/LogInForm.jsx
+++ frontend/src/components/Forms/LogInForm/LogInForm.jsx
@@ -8,6 +8,7 @@ import { classNames } from "primereact/utils";
 import { Divider } from "primereact/divider";
 import "./LogInForm.css";
 import { Container, Row, Col } from "react-bootstrap";
+import axios from "axios";
 
 export const LoginForm = () => {
 	const [formData, setFormData] = useState({});
@@ -15,13 +16,14 @@ export const LoginForm = () => {
 
 	const validate = (data) => {
 		let errors = {};
-		if (!data.email) {
-			errors.email = "Email is required.";
-		} else if (
-			!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)
-		) {
-			errors.email = "Invalid email address. E.g. example@email.com";
-		}
+		if (!data.username) {
+			errors.username = "Username is required.";
+		} 
+		// else if (
+			// !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)
+		// ) {
+		// 	errors.email = "Invalid email address. E.g. example@email.com";
+		// }
 		if (!data.password) {
 			errors.password = "Password is required.";
 		}
@@ -72,7 +74,7 @@ export const LoginForm = () => {
 								<Form
 									onSubmit={onSubmit}
 									initialValues={{
-										email: "",
+										username: "",
 										password: "",
 									}}
 									validate={validate}
@@ -82,13 +84,13 @@ export const LoginForm = () => {
 											className="p-fluid"
 										>
 											<Field
-												name="email"
+												name="username"
 												render={({ input, meta }) => (
 													<div className="field">
 														<span className="p-float-label p-input-icon-right">
 															<i className="pi pi-envelope" />
 															<InputText
-																id="email"
+																id="username"
 																{...input}
 																className={classNames(
 																	{
@@ -100,7 +102,7 @@ export const LoginForm = () => {
 																)}
 															/>
 															<label
-																htmlFor="email"
+																htmlFor="username"
 																className={classNames(
 																	{
 																		"p-error":
@@ -110,7 +112,7 @@ export const LoginForm = () => {
 																	}
 																)}
 															>
-																Email*
+																Username*
 															</label>
 														</span>
 														{getFormErrorMessage(
