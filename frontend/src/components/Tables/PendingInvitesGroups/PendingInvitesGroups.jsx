e7f35cc9d5a6fe0c5abcc59c802ce3e25ecb299f
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-08-24 16:36:05 -0400

    RBT-74 added dialog and styling

--- frontend/src/components/Tables/PendingInvitesGroups/PendingInvitesGroups.jsx
+++ frontend/src/components/Tables/PendingInvitesGroups/PendingInvitesGroups.jsx
@@ -3,7 +3,7 @@ import { DataTable } from "primereact/datatable";
 import { Button } from "primereact/button";
 import { Column } from "primereact/column";
 import { Dialog } from "primereact/dialog";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import "./PendingInvitesGroups.css";
 
 export const PendingInvitesGroups = () => {
@@ -23,22 +23,36 @@ export const PendingInvitesGroups = () => {
 	];
 	const dialogFooter = (
 		<div className="flex justify-content-center">
-			<Button
-				label="Hide Details"
-				className="p-button-text"
-				autoFocus
-				onClick={hideDetails}
-			/>
+			<Row>
+				<Col>
+					{/* TODO: add accept logic */}
+					<Button
+						label="Accept"
+						className="p-button-text"
+						autoFocus
+						onClick={hideDetails}
+					/>
+				</Col>
+				<Col></Col>
+				<Col>
+					{/* TODO: add decline login */}
+					<Button
+						label="Decline"
+						className="p-button-text"
+						onClick={hideDetails}
+					/>
+				</Col>
+			</Row>
 		</div>
 	);
 
 	return (
-		<Container as={Row} className="container-table-pending-invites">
+		<Container as={Row} className="container-table-pending-invites-groups">
 			<Dialog
 				visible={showMessage}
 				onHide={hideDetails}
+				showHeader={true}
 				footer={dialogFooter}
-				showHeader={false}
 				breakpoints={{ "960px": "80vw" }}
 				style={{ width: "30vw" }}
 			>
@@ -47,7 +61,7 @@ export const PendingInvitesGroups = () => {
 					<p style={{ lineHeight: 1.5 }}>Group info here</p>
 				</div>
 			</Dialog>
-			<h4>Pending Group Invitations</h4>
+			<h4>Group Invitations</h4>
 			<DataTable value={invites}>
 				<Column field="groupName" header="Group Name" />
 				<Column field="details" header="Details" />
