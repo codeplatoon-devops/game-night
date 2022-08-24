6c90955f42a2a7a4cda7b26351f06ca61ae0426c
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-08-24 15:53:30 -0400

    RBT-72 experimenting with details and dialog

--- frontend/src/components/Tables/PendingInvitesEvents/PendingInvitesEvents.jsx
+++ frontend/src/components/Tables/PendingInvitesEvents/PendingInvitesEvents.jsx
@@ -1,25 +1,55 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { DataTable } from "primereact/datatable";
+import { Button } from "primereact/button";
 import { Column } from "primereact/column";
+import { Dialog } from "primereact/dialog";
+import { Container } from "react-bootstrap";
 
 export const PendingInvitesEvents = () => {
+	const [showMessage, setShowMessage] = useState(false);
+	const onClose = () => {
+		setShowMessage(false);
+	};
 	const invites = [
 		{
 			eventName: "session zero",
 			category: "DnD",
 			location: "city state",
 			date: "24 Aug 2022",
-			description: "fun game night",
+			details: <Button onClick={onClose}>laksfjwefoi</Button>,
 		},
 	];
+	// const dialogFooter = (
+	// 	<div className="flex justify-content-center">
+	// 		<Button
+	// 			label="OK"
+	// 			className="p-button-text"
+	// 			autoFocus
+	// 			onClick={setShowMessage(false)}
+	// 		/>
+	// 	</div>
+	// );
 
 	return (
-		<DataTable value={invites}>
-			<Column field="eventName" header="Event Name" />
-			<Column field="category" header="Category" />
-			<Column field="description" header="Description" />
-			<Column field="location" header="Location" />
-			<Column field="date" header="Date" />
-		</DataTable>
+		<Container>
+			{/* <Dialog
+				visible={showMessage}
+				onHide={() => setShowMessage(false)}
+				footer={dialogFooter}
+				showHeader={false}
+				breakpoints={{ "960px": "80vw" }}
+				style={{ width: "30vw" }}
+			>
+				<div className="flex align-items-center flex-column pt-6 px-3 field">
+					<h5>Invite Details</h5>
+					<p style={{ lineHeight: 1.5 }}>Event info here</p>
+				</div>
+			</Dialog> */}
+			<DataTable value={invites}>
+				<Column field="eventName" header="Event Name" />
+				<Column field="category" header="Category" />
+				<Column field="details" header="Description" />
+			</DataTable>
+		</Container>
 	);
 };
