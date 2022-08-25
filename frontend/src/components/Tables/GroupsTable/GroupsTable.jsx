f635843c68338a23518c44f0bdf64e8f3f20ecab
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-08-25 15:36:58 -0400

    RBT-86 updated Groups list with buttons and correct data

--- frontend/src/components/Tables/GroupsTable/GroupsTable.jsx
+++ frontend/src/components/Tables/GroupsTable/GroupsTable.jsx
@@ -1,17 +1,27 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Accordion, AccordionTab } from "primereact/accordion";
 import { Button } from "primereact/button";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 import "./GroupsTable.css";
 
 export default function GroupsTable(props) {
 	const [activeIndex, setActiveIndex] = useState(null);
-	const formatGroup = () => {
-		let returnGroups = "";
+	let groups = [];
+	if (props.groups) {
 		for (let group of props.groups) {
-			returnGroups += group + "\n";
+			let tempGroup = {
+				groupName: <Button className="p-button-text">{group}</Button>,
+			};
+
+			groups.push(tempGroup);
 		}
-		return returnGroups;
-	};
+	}
+	const noGroups = [
+		{
+			groupName: "None",
+		},
+	];
 	return (
 		<Accordion
 			activeIndex={activeIndex}
@@ -19,7 +29,10 @@ export default function GroupsTable(props) {
 			className="table-groups-list"
 		>
 			<AccordionTab header="Your Groups">
-				<ul>{props.groups ? formatGroup() : "None"}</ul>
+				{/* <ul>{props.groups ? formatGroup() : "None"}</ul> */}
+				<DataTable value={props.groups ? groups : noGroups}>
+					<Column field="groupName" />
+				</DataTable>
 			</AccordionTab>
 		</Accordion>
 	);
