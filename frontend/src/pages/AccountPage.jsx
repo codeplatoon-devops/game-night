2e285cae9f7fb017037a60cb76e82c671fbfdbe0
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-08-25 10:07:51 -0400

    RBT-79 added pending invites to account page

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,10 +1,11 @@
-
 import { PendingInvitesEvents } from "../components/Tables/PendingInvitesEvents/PendingInvitesEvents";
+import { PendingInvitesGroups } from "../components/Tables/PendingInvitesGroups/PendingInvitesGroups";
 
 export default function AccountPage() {
 	return (
 		<div>
 			<PendingInvitesEvents />
+			<PendingInvitesGroups />
 		</div>
 	);
 }
