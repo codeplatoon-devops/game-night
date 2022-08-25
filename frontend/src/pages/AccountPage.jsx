3d0564d7647908e1d7cb1e6226116a01c4060519
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-08-25 13:44:00 -0400

    RBT-88 updated pending events table and temporarily added to Accounts page

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,7 +1,10 @@
+import { PendingInvitesEvents } from "../components/Tables/PendingInvitesEvents/PendingInvitesEvents";
+
 export default function AccountPage() {
 	return (
 		<div>
 			<h1>Account page</h1>
+			<PendingInvitesEvents />
 		</div>
 	);
 }
