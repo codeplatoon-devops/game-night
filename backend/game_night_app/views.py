790ecebdaf4de2db291e238723efde1cb5da9a23
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-08-27 17:58:30 +0200

    added attending events to calendar and upcoming

--- backend/game_night_app/views.py
+++ backend/game_night_app/views.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 from .models import AppUser, Event, EventGame, EventRequest, EventUser, Group, GroupList, GroupRequest
 from django.forms.models import model_to_dict
 from django.contrib.auth.decorators import login_required
+from itertools import chain
 
 
 load_dotenv()
@@ -371,9 +372,13 @@ def create_event(request):
 @api_view(['GET'])
 def userevents(request):
     if request.user.is_authenticated:
-        events = Event.objects.filter(owner=request.user.id)
-        data = serializers.serialize('json',events)
-        # print(data)
+        events_owner = Event.objects.filter(owner=request.user.id)
+        owned_ids = events_owner.values_list('id', flat='True')
+        all_events_attending = EventUser.objects.filter(attendee=request.user.id).exclude(event__in=owned_ids)
+        attending_ids = all_events_attending.values_list('event', flat='True')
+        events_attending = Event.objects.filter(id__in=attending_ids)
+        user_events = list(chain(events_owner,events_attending))
+        data = serializers.serialize('json', user_events)
  
         return HttpResponse(data, content_type='application/json')
     else:
@@ -384,7 +389,8 @@ def userevents_byid(request,id):
         code = str(id)
         if len(code) < 8:
             code = code.rjust(8,'0')
-        events = Event.objects.filter(owner = request.user.id).filter(code=code)
+        # removed the check that only lets you get events you own
+        events = Event.objects.filter(code=code) 
         data = serializers.serialize('json',events)
  
         return HttpResponse(data, content_type='application/json')
@@ -394,8 +400,13 @@ def userevents_byid(request,id):
 @api_view(['GET'])
 def user_events_table_data(request):
     if request.user.is_authenticated:
-        events = Event.objects.filter(owner = request.user.id)
-        data = serializers.serialize('json', events, fields=['name', 'category', 'description', 'address_1', 'start_time'])
+        events_owner = Event.objects.filter(owner = request.user.id)
+        owned_ids = events_owner.values_list('id', flat='True')
+        all_events_attending = EventUser.objects.filter(attendee=request.user.id).exclude(event__in=owned_ids)
+        attending_ids = all_events_attending.values_list('event', flat='True')
+        events_attending = Event.objects.filter(id__in=attending_ids)
+        user_events = list(chain(events_owner,events_attending))
+        data = serializers.serialize('json', user_events, fields=['name', 'category', 'description', 'address_1', 'start_time'])
         return HttpResponse(data, content_type='application/json')
     else:
         return JsonResponse({'user': False})
