9215628ea0c9348ac894bdd6606f2d3d1faa66c3
kayleeburch <kayleemburch@gmail.com>
2022-08-27 23:09:31 -0400

    RBT-101 added in information but need to find out current attendees

--- frontend/src/pages/EventDetailPage.jsx
+++ frontend/src/pages/EventDetailPage.jsx
@@ -1,5 +1,39 @@
-import {EventDetails} from '../components/EventDetails/EventDetail'
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom'
+import moment from 'moment';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import axios from 'axios';
+import { Button } from 'react-bootstrap'
+import EventDetailList from "../components/List/EventDetailList"
 
 export default function EventDetailPage() {
-	return  <EventDetails />;
+
+	let { eventId } = useParams()
+
+    const [eventDetail, setEventDetail] = useState(null)
+	const [games, setGames] = useState(null)
+	const [startTime, setStartTime] = useState(null)
+	const [endTime, setEndTime] = useState(null)
+
+    useEffect(() => {
+		axios
+		.get(`/userevents/${eventId}`)
+		.then((response) => {
+            console.log('response data', response.data[0].fields)
+			setGames(Object.values(response.data[0].fields.games))
+			setStartTime(moment(response.data[0].fields.start_time).format('MMMM Do YYYY, h:mm a'))
+			setEndTime(moment(response.data[0].fields.end_time).format('MMMM Do YYYY, h:mm a'))
+			setEventDetail(response.data[0].fields)
+		})
+	}, [])
+
+    return(
+        <div>
+		{eventDetail &&
+			<EventDetailList eventDetail={eventDetail} games={games} startTime={startTime} endTime={endTime}/>
+		}
+        </div>
+    )
 }
