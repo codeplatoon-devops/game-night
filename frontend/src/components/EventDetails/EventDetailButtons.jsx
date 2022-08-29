be6b201cffe4a92cdee819e32655a7bb76de6c0d
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2022-08-29 19:27:47 -0400

    started delete/leave group

--- frontend/src/components/EventDetails/EventDetailButtons.jsx
+++ frontend/src/components/EventDetails/EventDetailButtons.jsx
@@ -4,20 +4,30 @@ import axios from "axios";
 
 function EventDetailButtons({user}) {
 
-    // const leaveGroup= function (name, code) {
-	// 	axios.put('/event/delete', {code:code})
-	// 		.then((response)=> {
-	// 			console.log('leave group response.data', response.data)
-	// 			props.setLeaveGroupInformation([name,code])
-	// 			if (response.data.group_deleted) {
-	// 				let channelId = "GroupChatroom";
-	// 				let channelName = name
-	// 				channelName += " Chatroom";
-	// 				channelId += code.toString();
-	// 				setDeleteChannelInformation([channelId,channelName])
-	// 			}
-	// 		})
-	// }
+    const deleteEvent= function () {
+        let event_id = eventDetail.id
+		axios.put('/event/delete',{id:event_id})
+			.then((response)=> {
+				console.log('delete group response.data', response.data)
+				props.setLeaveGroupInformation([name,code])
+				if (response.data.group_deleted) {
+					let channelId = "GroupChatroom";
+					let channelName = name
+					channelName += " Chatroom";
+					channelId += code.toString();
+					setDeleteChannelInformation([channelId,channelName])
+				}
+			})
+	}
+
+    const leaveEvent= function () {
+        let event_id = eventDetail.id
+		axios.put('/event/leave', {code:code})
+			.then((response)=> {
+				console.log('leave group response.data', response.data)
+				props.setLeaveGroupInformation([name,code])
+			})
+	}
 
 
     return(
