9f0562cbc9caf015507a3f22aa54f2304e440ac2
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-08-30 11:02:29 -0400

    RBT-159 removed signup prompt when logged in

--- frontend/src/components/Tables/GroupsTable/GroupsTable.jsx
+++ frontend/src/components/Tables/GroupsTable/GroupsTable.jsx
@@ -6,11 +6,13 @@ import { Panel } from "primereact/panel";
 // import './TabViewDemo.css';
 import "./GroupsTable.css";
 import axios from "axios";
+import { Accordion, AccordionTab } from "primereact/accordion";
 
 export default function GroupsTable(props) {
 	// const [activeIndex2, setActiveIndex2] = useState(0)
 	// const scrollableTabs = Array.from({ length: 50 }, (_, i) => ({ title: `Tab ${i + 1}`, content: `Tab ${i + 1} Content` }));
 	const [selectedGroup, setSelectedGroup] = useState(null);
+	const [activeIndex, setActiveIndex] = useState(null);
 	let groups = [];
 	// let scrollGroups=[]
 	// props.groups now has group name and code
@@ -19,6 +21,7 @@ export default function GroupsTable(props) {
 			let tempGroup = {
 				groupName: group[0],
 				// group[2] = all the other members
+				groupMembers: group[2],
 			};
 			// let singleGroup= {
 			// 	title: group[0],
@@ -32,7 +35,7 @@ export default function GroupsTable(props) {
 		groups = [{ groupName: "None" }];
 	}
 	const handleChange = () => {
-		console.log("clicked group");
+		console.log(selectedGroup);
 		// get members of the group
 		// return(
 		// <Button onClick={leaveGroup}>Leave Group</Button>
@@ -40,24 +43,25 @@ export default function GroupsTable(props) {
 		// console.log("clicked group");
 	};
 
-	const leaveGroup= function () {
-		axios.put()
-	}
+	const leaveGroup = function () {
+		axios.put();
+	};
 
 	return (
 		<div>
-		<Panel header="Your Groups">
-			<ListBox
-				value={selectedGroup}
-				options={groups}
-				onChange={handleChange}
-				optionLabel="groupName"
-				style={{ width: "100%" }}
-				// itemTemplate={eventsTemplate}
-				listStyle={{ maxHeight: "250px" }}
-			/>
-		</Panel>
-		{/* <div className="card">
+			<Panel header="Your Groups">
+				<ListBox
+					value={selectedGroup}
+					options={groups}
+					onChange={(e) => setSelectedGroup(e.value)}
+					onClick={handleChange}
+					optionLabel="groupName"
+					optionValue="groupMembers"
+					style={{ width: "100%" }}
+					listStyle={{ maxHeight: "250px" }}
+				/>
+			</Panel>
+			{/* <div className="card">
 		<h5>Your Groups</h5>
 		<TabView activeIndex={activeIndex2} onTabChange={(e) => setActiveIndex2(e.index)} scrollable>
 			{scrollGroups.map((tab) => {
@@ -69,6 +73,6 @@ export default function GroupsTable(props) {
 			})}
 		</TabView>
 	</div> */}
-	</div>
+		</div>
 	);
 }
