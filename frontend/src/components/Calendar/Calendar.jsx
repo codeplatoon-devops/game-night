c0e34bb413ac30f5707ed8ce4f7e358ef45e94bd
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-08-31 11:44:29 +0200

    updating info in container

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -2,9 +2,13 @@ import FullCalendar, { ViewApi } from "@fullcalendar/react";
 import dayGridPlugin, { DayGridView } from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./Calendar.css";
 
-export default function Calendar({ data }) {
+export default function Calendar() {
+
+	const [events, setEvents] = useState([]);
 	// source of data-date confusion :(
 	const handleDateClick = (date) => {
 		let calendarApi = date.view.calendar;
@@ -19,6 +23,18 @@ export default function Calendar({ data }) {
 	const handleEventClick = (event) => {
 		// console.log(event.event._def);
 	};
+	useEffect(() => {
+		axios
+			.get("/userevents")
+			.then((response) => {
+			
+				setEvents(response.data);
+				// console.log(response.data)
+			})
+			.catch((error) => {
+				// console.log("ERROR", error);
+			});
+	}, []);
 
 	return (
 		<div className="container-fluid calendar">
@@ -32,7 +48,7 @@ export default function Calendar({ data }) {
 					center: "title",
 					right: "dayGridMonth,timeGridWeek,timeGridDay",
 				}}
-				events={data.map((event) => ({
+				events={events.map((event) => ({
 					title: event.name,
 					start: event.start_time,
 					end: event.end_time,
