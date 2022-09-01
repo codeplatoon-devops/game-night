8bca64f043027938c82abb628e89122ccf594e2b
alishaburgfeld <98989927+alishaburgfeld@users.noreply.github.com>
2022-09-01 19:11:46 -0400

    should work now

--- frontend/src/components/Tables/PendingInvitesGroups/PendingInvitesGroups.jsx
+++ frontend/src/components/Tables/PendingInvitesGroups/PendingInvitesGroups.jsx
@@ -51,6 +51,7 @@ export const PendingInvitesGroups = (props) => {
 					// window.alert("Group joined!");
 					setShowAcceptedMessage(true);
 					props.setJoinGroupInformation([name, code]);
+					// reloading
 					window.location.reload()
 				} else {
 					window.alert(`${response.data.reason}`);
